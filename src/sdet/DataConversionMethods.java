0349223fe97dc8deacfa71e7630dbe4ed8a599e2
Zadid-Ahsan5274 <zadid.ahsan5274@gmail.com>
2026-07-08 11:52:31 +0600

    Test Upload

---
+++ src/sdet/DataConversionMethods.java
@@ -0,0 +1,18 @@
+package sdet;
+
+public class DataConversionMethods {
+    public static void main(String[] args){
+
+        // String to int
+        // Can not convert to int
+        /* String s = "Welcome";
+        int stringInt = Integer.parseInt(s); */
+
+        String s = "12345";
+        int stringInt = Integer.parseInt(s);
+        System.out.println("String to int: " + stringInt);
+        
+
+
+    }
+}
